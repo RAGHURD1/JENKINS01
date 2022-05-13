7efd93d8ce3ecbc2f303565bed4ba783454de0f8
RAGHURD1 <96580461+RAGHURD1@users.noreply.github.com>
2022-05-13 13:47:35 +0530

    first commit

---
+++ Organization/src/test/java/organization1/CreateOrganizationTest.java
@@ -0,0 +1,21 @@
+package organization1;
+
+import org.junit.Test;
+
+public class CreateOrganizationTest {
+@Test
+	
+	public void CreateOrganizationTest() {
+		
+		System.out.println("execute create org test");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("browser");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		
+	}
+
+}
+
+
+
